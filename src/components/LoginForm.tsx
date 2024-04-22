b7190c5e0afd4572ab44d242e410d8dae4ad3231
novaepitech <mateo.cabrera@epitech.eu>
2024-04-22 18:28:01 +0200

    feat: dynamic import folders under src via @/

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
-import { cn } from "../utils/cn";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/utils/cn";
 
 export function LoginForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {

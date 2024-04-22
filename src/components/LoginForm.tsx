1448d8e5ddb74281fc44c1d12164199f8c6e26f7
novaepitech <mateo.cabrera@epitech.eu>
2024-04-22 19:15:02 +0200

    fix: no background for login form on mobile

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -11,7 +11,7 @@ export function LoginForm() {
     window.location.href = "/browse";
   };
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input md:bg-white md:dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome back !
       </h2>

b0430706cdd31264c24ea798a581709c373b1732
novaepitech <mateo.cabrera@epitech.eu>
2024-05-09 11:56:10 +0200

    fix: API retries to connect to db until successfull

--- backend/api/database/connect.go
+++ backend/api/database/connect.go
@@ -4,6 +4,7 @@ import (
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/postgres"
     "log"
+    "time"
     "api/models"
 )
 
@@ -12,15 +13,19 @@ var db *gorm.DB
 func ConnectDB() {
     var err error
     ***REMOVED***
-    db, err = gorm.Open("postgres", dataSourceName)
-    if err != nil {
-        log.Fatal(err)
+    for {
+        db, err = gorm.Open("postgres", dataSourceName)
+        if err != nil {
+            log.Println("Failed to connect to the database:", err)
+            time.Sleep(5 * time.Second)
+            continue
+        }
+        db.AutoMigrate(&models.User{}, &models.Media{}, &models.Favoris{}, &models.Likes{})
+        log.Println("Connexion à la base de données établie et migration automatique effectuée.")
+        break
     }
-    db.AutoMigrate(&models.User{}, &models.Media{}, &models.Favoris{}, &models.Likes{})
-    log.Println("Connexion à la base de données établie et migration automatique effectuée.")
 }
 
 func GetDB() *gorm.DB {
     return db
 }
-

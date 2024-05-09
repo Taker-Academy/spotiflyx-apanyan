6eb617d264e18d48d404ba5ec3ec2c50180f70d4
novaepitech <mateo.cabrera@epitech.eu>
2024-05-09 12:34:37 +0200

    fix: check if user token is null for additional dashboard security

--- frontend/src/app/auth/useCheckAuth.tsx
+++ frontend/src/app/auth/useCheckAuth.tsx
@@ -4,7 +4,8 @@ import { useEffect } from 'react';
 export function useCheckAuth(token: string | null) {
   const { replace } = useRouter();
   useEffect(() => {
-    if (!token) {
+    console.log('token', token);
+    if (!token || token === 'null') {
       replace('/login'); // redirect to login if user is not authenticated
     }
   }, [token, replace]);

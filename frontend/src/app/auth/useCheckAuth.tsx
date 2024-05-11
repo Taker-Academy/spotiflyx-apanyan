f5dd90683b19a6fadb02d9c9587500ef749aa731
novaepitech <mateo.cabrera@epitech.eu>
2024-05-11 14:29:08 +0200

    fix: check if /user/me is valid instead ok present token for more security

--- frontend/src/app/auth/useCheckAuth.tsx
+++ frontend/src/app/auth/useCheckAuth.tsx
@@ -1,11 +1,26 @@
+import useSWR from 'swr';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 export function useCheckAuth(token: string | null) {
   const { replace } = useRouter();
-  useEffect(() => {
-    if (!token || token === 'null') {
-      replace('/login'); // redirect to login if user is not authenticated
+
+  const { data, error } = useSWR('http://127.0.0.1:8080/user/me', async url => {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('User is not authenticated');
     }
-  }, [token, replace]);
+
+    return response.json();
+  });
+
+  if (error) {
+    replace('/login');
+  }
+
+  return { user: data, isLoading: !error && !data, isError: error };
 }

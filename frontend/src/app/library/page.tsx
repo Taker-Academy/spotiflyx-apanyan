2ef567696a461908295af7689687234f91c9b8bf
novaepitech <mateo.cabrera@epitech.eu>
2024-05-15 17:29:33 +0200

    feat: basic library UI page

--- frontend/src/app/library/page.tsx
+++ frontend/src/app/library/page.tsx
@@ -1,11 +1,32 @@
 "use client"
 
 import DefaultLayout from '@/components/DefaultLayout';
+import FavoriteMedias from '@/components/FavoriteMedias';
+import LikedMedias from '@/components/LikedMedias';
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
 
 export default function Library() {
     return (
         <DefaultLayout currentPage='Library'>
-            <h1>Library Page</h1>
+            <Tabs defaultValue="liked">
+                <div className="flex items-center">
+                    <TabsList className='ml-8'>
+                        <TabsTrigger value="liked" className='w-[100px]'>Liked</TabsTrigger>
+                        <TabsTrigger value="favorite" className='w-[100px]'>Favorite</TabsTrigger>
+                    </TabsList>
+                </div>
+                <TabsContent value="liked">
+                    <LikedMedias />
+                </TabsContent>
+                <TabsContent value="favorite">
+                    <FavoriteMedias />
+                </TabsContent>
+            </Tabs>
         </DefaultLayout>
     )
 }
---
+++ frontend/src/components/FavoriteMedias.tsx
@@ -0,0 +1,71 @@
+import useSWR, { mutate } from 'swr';
+import useLocalStorage from '@/app/auth/useLocalStorage';
+import { Media } from '@/app/types';
+import { Link } from "next-view-transitions";
+
+async function fetcher([url, token]: [string, string]) {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('An error occurred while fetching the media data.');
+  }
+
+  const data = await response.json();
+
+  return data.data;
+}
+
+export default function FavoriteMedias() {
+  const [token] = useLocalStorage('token', null);
+  const { data: medias, error } = useSWR(['http://127.0.0.1:8080/media', token], fetcher);
+
+  if (error) return <div>Error: {error.message}</div>;
+  if (!medias) return <h1 className="text-3xl">Favorite medias</h1>;
+
+  return (
+    <main className="mt-4 flex flex-wrap justify-evenly">
+      <section className='mx-8'>
+        <h1 className="text-3xl text-center mb-8">Favorite videos</h1>
+        <main className='space-y-8'>
+          {medias.filter((media: { type: string; }) => media.type === 'video').map((media: Media) => (
+            <div key={media.id}>
+              <h2 className='text-2xl font-semibold mb-2 ml-4'>{media.title}</h2>
+              <div className='flex items-center gap-6 relative'>
+                <Link href={`/video/${media.id}`} className="absolute w-full h-full top-0 left-0 z-10"></Link>
+                <iframe
+                  className='video-iframe'
+                  width="325"
+                  height="180"
+                  src={`https://www.youtube.com/embed/${media.mediaid}`}
+                  title="YouTube video player"
+                ></iframe>
+              </div>
+            </div>
+          ))}
+        </main>
+      </section>
+      <section className='mx-8'>
+        <h1 className="text-3xl text-center mb-8">Favorite musics</h1>
+        {medias.filter((media: { type: string; }) => media.type === 'music').map((media: Media) => (
+          <div key={media.id}>
+            <h2 className='text-2xl font-semibold mb-2 ml-4'>{media.title}</h2>
+            <div className='flex items-center gap-6 relative'>
+              <Link href={`/music/${media.id}`} className="absolute w-full h-full top-0 left-0 z-10"></Link>
+              <iframe
+                className='music-iframe'
+                src={`https://open.spotify.com/embed/track/${media.mediaid}`}
+                width="325"
+                height="180"
+              >
+              </iframe>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
